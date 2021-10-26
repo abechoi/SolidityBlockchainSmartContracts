c2065ed131abfef6d0c6a8090015825ad5b07e91
Abraham Choi <abrahamchoi@achoi14863-789.lan>
2021-10-26 14:37:55 -0700

    added SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = false;
+    string favoriteString = "String";
+    int256 favoriteInt = -5;
+    address favoriteAddress = 0x4C19eE0903fcCc960FD2e4463ab739fdCD3b6088;
+}
